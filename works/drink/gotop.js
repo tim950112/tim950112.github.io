56b404a5bde599cedf09568c5417f3808b5d512f
tim950112 <tim950112@gmail.com>
2021-08-25 13:43:52 +0800

    作品6上傳
    作品6上傳

---
+++ works/drink/gotop.js
@@ -0,0 +1,37 @@
+//回到頂端按鈕 start 
+// JavaScript Document
+//<![CDATA[
+(function() {
+    $("body").append("<img id='goTopButton' style='display: none; z-index: 5; cursor: pointer;' title='回到頂端'/>");
+    var img = "images/top.png",
+        locatioin = 2 / 3, // 按鈕出現在螢幕的高度
+        right = 20, // 距離右邊 px 值
+        opacity = 0.6, // 透明度
+        speed = 500, // 捲動速度
+        $button = $("#goTopButton"),
+        $body = $(document),
+        $win = $(window);
+    $button.attr("src", img);
+    $button.on({
+        mouseover: function() { $button.css("opacity", 1); },
+        mouseout: function() { $button.css("opacity", opacity); },
+        click: function() { $("html, body").animate({ scrollTop: 0 }, speed); }
+    });
+    window.goTopMove = function() {
+        var scrollH = $body.scrollTop(),
+            winH = $win.height(),
+            css = { "top": winH * locatioin + "px", "position": "fixed", "right": right, "opacity": opacity, "width": "75px" };
+        if (scrollH > 20) {
+            $button.css(css);
+            $button.fadeIn("slow");
+        } else {
+            $button.fadeOut("slow");
+        }
+    };
+    $win.on({
+        scroll: function() { goTopMove(); },
+        resize: function() { goTopMove(); }
+    });
+})();
+//]]>
+//回到頂端按鈕 end
