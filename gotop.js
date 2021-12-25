543a0b631f472a02259266fcf8d8cfd353f32b82
tim950112 <tim950112@gmail.com>
2021-12-25 12:56:51 +0800

    新增gotop按鈕以及日夜切換連絡資訊圖片
    新增gotop按鈕以及日夜切換連絡資訊圖片

---
+++ gotop.js
@@ -0,0 +1,37 @@
+//回到頂端按鈕 start 
+// JavaScript Document
+//<![CDATA[
+(function() {
+    $("body").append("<img id='goTopButton' style='display: none; z-index: 5; cursor: pointer;' title='回到頂端'/>");
+    var img = "/images/upload.png",
+        locatioin = 4 / 5, // 按鈕出現在螢幕的高度比例
+        right = 30, // 距離右邊 px 值
+        opacity = 0.6, // 透明度
+        speed = 500, // 捲動速度
+        $button = $("#goTopButton"),
+        $body = $(document),
+        $win = $(window);
+    $button.attr("src", img);
+    $button.on({
+        mouseover: function() { $button.css("opacity", 1); },
+        mouseout: function() { $button.css("opacity", opacity); },
+        click: function() { $("html, body").animate({ scrollTop: 0 }, speed); }
+    });
+    window.goTopMove = function() {
+        var scrollH = $body.scrollTop(),
+            winH = $win.height(),
+            css = { "top": winH * locatioin + "px", "position": "fixed", "right": right, "opacity": opacity, "width": "75px" };
+        if (scrollH > 20) {
+            $button.css(css);
+            $button.fadeIn("slow");
+        } else {
+            $button.fadeOut("slow");
+        }
+    };
+    $win.on({
+        scroll: function() { goTopMove(); },
+        resize: function() { goTopMove(); }
+    });
+})();
+//]]>
+//回到頂端按鈕 end
